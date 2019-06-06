1aad77ae6e596eaf46bbafe372dd04ebf8f218f4
Chinh Le <chinh.le11@gmail.com>
2019-06-05 21:29:39 -0400

    preparing the demo project

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import './Backdrop.css';
 
-const backdrop = (props) => (
-    <div className="Backdrop"></div>
-);
+const backdrop = (props) => {
+    const cssClasses = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClosed'];
+    
+    return <div className={cssClasses.join(' ')} onClick={props.closed}></div>
+};
 
 export default backdrop;
